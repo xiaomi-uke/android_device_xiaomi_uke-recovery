aac1f07568702c4d55d7415dc913c9690b348933
AzzyC <azmath2000@gmail.com>
2025-11-06 21:51:22 +0000

    peridot: Preserve recovery.log before forced reboot
    - Due to immediate forced reboot, usual log backup mechanism will not work
    - To not lose debugging information, backup recovery.log with a timestamp

--- recovery/root/system/bin/post_rom_flash.sh
+++ recovery/root/system/bin/post_rom_flash.sh
@@ -19,6 +19,9 @@ LOGMSG "Setting instructions for next reboot..."
 echo "install /FFiles/DFE.zip" > /cache/recovery/openrecoveryscript
 echo "noForceReboot" >> /cache/recovery/openrecoveryscript
 
+LOGMSG "Preserving recovery.log before recovery reboot..."
 LOGMSG "---$SCRIPT_NAME end---"
+mkdir -p /sdcard/Fox/logs
+cp /tmp/recovery.log "/sdcard/Fox/logs/recovery_$(date +"%Y%m%d_%H%M%S").log"
 
 reboot recovery
