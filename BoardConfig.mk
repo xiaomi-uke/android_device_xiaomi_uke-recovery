7c7d060c151f765ac64264b073655580f59388df
AzzyC <azmath2000@gmail.com>
2025-12-28 17:57:16 +0000

    peridot: SecureElement: Load in NFC NXP module
    - Address: "android.hardware.secure_element-service.qti: eseGetVendorId: Unknown eSE HW"

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,7 +180,7 @@ else
 endif
 
 TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
-TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko focaltech_3683g.ko focaltech_touch.ko goodix_core.ko goodix_ts.ko qti_battery_charger.ko xiaomi_touch.ko"
+TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko focaltech_3683g.ko focaltech_touch.ko goodix_core.ko goodix_ts.ko nxp-nci.ko qti_battery_charger.ko xiaomi_touch.ko"
 TW_LOAD_VENDOR_MODULES_EXCLUDE_GKI := true
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone48/temp"
 TW_BATTERY_SYSFS_WAIT_SECONDS := 6
