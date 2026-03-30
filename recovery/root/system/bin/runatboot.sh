46ce99cac950017b3823ea9fc2768d7f4aa6d45c
AzzyC <azmath2000@gmail.com>
2026-03-30 19:22:31 +0100

    peridot: Use source's log pruning function
    - Script function now available at source; remove local duplicate:
    https://gitlab.com/OrangeFox/vendor/recovery/-/commit/bb0ac73

--- recovery/root/system/bin/runatboot.sh
+++ recovery/root/system/bin/runatboot.sh
@@ -13,25 +13,11 @@ reset_touch() {
 	echo 1 > /sys/devices/platform/goodix_ts.0/reset
 }
 
-purge_logs() {
-	LOGMSG "Purging recovery-generated logs older than 14 days..."
-
-	FOX_LOG_DIR="/persist/Fox/logs"
-
-	if [ -d "$FOX_LOG_DIR" ]; then
-		find "$FOX_LOG_DIR" -maxdepth 1 -type f -mtime +14 -print -delete
-	else
-		LOGMSG "$FOX_LOG_DIR not present"
-	fi
-}
-
 SCRIPT_NAME="$(basename "$0")"
 
 LOGMSG "---$SCRIPT_NAME start---"
 
 reset_touch
-
-purge_logs
 
 LOGMSG "---$SCRIPT_NAME end---"
 exit 0
